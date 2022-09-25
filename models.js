078f1cd1040193359b28e3989513699dc68c15f8
Nazim Leulmi <naz@nazimleulmi.com>
2022-09-25 06:29:23 +0000

    New API routes

--- models.js
+++ models.js
@@ -14,7 +14,7 @@ const userSchema = new Schema(
     bio: {
       type: String,
       required: true,
-      default: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
+      default: `Tell us about your self in less than 300 letters , so your followers can get to know you`,
     },
     isoCode: { type: String, required: true },
     dialCode: { type: String, required: true },
